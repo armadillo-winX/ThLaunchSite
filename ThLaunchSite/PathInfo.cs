1c879791782e082bce886ea3ae1de4e1b4bbaed1
Mashiro Tamane <80902436+armadillo-winX@users.noreply.github.com>
2023-07-23 17:32:11 +0900

    PathInfoクラス追加

---
+++ ThLaunchSite/PathInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThLaunchSite
+{
+    internal class PathInfo
+    {
+        public static string AppPath => typeof(App).Assembly.Location;
+
+        public static string? AppLocation => Path.GetDirectoryName(AppPath);
+    }
+}
