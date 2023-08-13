295eacd67263557267b713c26fb1f4f0737f075c
Mashiro Tamane <80902436+armadillo-winX@users.noreply.github.com>
2023-08-14 00:15:12 +0900

    東方動作記録ファイルのパスを取得するメソッドを実装

--- ThLaunchSite/Game/GameFile.cs
+++ ThLaunchSite/Game/GameFile.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ThLaunchSite.Game
+﻿namespace ThLaunchSite.Game
 {
     internal class GameFile
     {
+        public static string? GetGameOperationLogFilePath(string gameId)
+        {
+            string gamePath = GamePath.GetGamePath(gameId);
+            if (gamePath == null)
+            {
+                return null;
+            }
+            else
+            {
+                string gameDirectory = Path.GetDirectoryName(gamePath);
+
+                if (
+                    gameId == GameIndex.Th06 ||
+                    gameId == GameIndex.Th07 ||
+                    gameId == GameIndex.Th08 ||
+                    gameId == GameIndex.Th09 ||
+                    gameId == GameIndex.Th10 ||
+                    gameId == GameIndex.Th11 ||
+                    gameId == GameIndex.Th12)
+                {
+                    return $"{gameDirectory}\\log.txt";
+                }
+                else
+                {
+                    return $"{PathInfo.ShanghaiAliceAppData}\\{gameId.ToLower()}\\log.txt";
+                }
+            }
+        }
     }
 }
