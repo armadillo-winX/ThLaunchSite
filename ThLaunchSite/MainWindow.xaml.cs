fa4a71e4703b824e508a641ee15b3c16c27ee53c
Mashiro Tamane <80902436+armadillo-winX@users.noreply.github.com>
2023-09-23 01:11:12 +0900

    不要なusingを削除

--- ThLaunchSite/MainWindow.xaml.cs
+++ ThLaunchSite/MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
