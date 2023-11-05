d4aac7e736865593e771fb370b0686561591d566
Mashiro Tamane <80902436+armadillo-winX@users.noreply.github.com>
2023-11-05 10:23:12 +0900

    不要なメソッドの削除

--- ThLaunchSite/Game/GameFile.cs
+++ ThLaunchSite/Game/GameFile.cs
@@ -130,55 +130,5 @@ namespace ThLaunchSite.Game
 
             return thpracFiles;
         }
-
-        public static string? GetGameOperationLogFilePath(string gameId)
-        {
-            string gamePath = GetGameFilePath(gameId);
-            if (gamePath == null)
-            {
-                return null;
-            }
-            else
-            {
-                string gameDirectory = Path.GetDirectoryName(gamePath);
-
-                if (
-                    gameId == GameIndex.Th06 ||
-                    gameId == GameIndex.Th07 ||
-                    gameId == GameIndex.Th08 ||
-                    gameId == GameIndex.Th09 ||
-                    gameId == GameIndex.Th10 ||
-                    gameId == GameIndex.Th11 ||
-                    gameId == GameIndex.Th12)
-                {
-                    return $"{gameDirectory}\\log.txt";
-                }
-                else
-                {
-                    return $"{PathInfo.ShanghaiAliceAppData}\\{gameId.ToLower()}\\log.txt";
-                }
-            }
-        }
-
-        public static string? GetGameOperationLogData(string gameId)
-        {
-            string? gameOperationLogFilePath = GetGameOperationLogFilePath(gameId);
-
-            if (File.Exists(gameOperationLogFilePath))
-            {
-                //Shift_JISに対応させる
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-                StreamReader streamReader = new(gameOperationLogFilePath, Encoding.GetEncoding("shift_jis"));
-                string gameOperationLogData = streamReader.ReadToEnd();
-                streamReader.Close();
-
-                return gameOperationLogData;
-            }
-            else
-            {
-                return "動作記録ファイルが見つかりませんでした。";
-            }
-        }
     }
 }
