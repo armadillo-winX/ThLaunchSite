d2f1b40a3c73eb12c2ea75fff93962909b9cc230
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-03-16 11:16:02 +0900

    GetPrivateProfileString()、GetPrivateProfileInt()を定義

--- ThLaunchSite/IniFileEditor.cs
+++ ThLaunchSite/IniFileEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,23 @@ namespace ThLaunchSite
 {
     internal class IniFileEditor
     {
+        //---DLL関数の定義 START---//
+        //GetPrivateProfileString関数の定義
+        [DllImport("KERNEL32.DLL")]
+        public static extern uint GetPrivateProfileString(
+            string lpAppName,
+            string lpKeyName,
+            string lpDefault,
+            StringBuilder lpReturnedString,
+            uint nSize,
+            string lpFileName);
+
+        //GetPrivateProfileInt関数の定義
+        [DllImport("KERNEL32.DLL")]
+        public static extern uint GetPrivateProfileInt(
+            string lpAppName,
+            string lpKeyName,
+            int nDefault,
+            string lpFileName);
     }
 }
