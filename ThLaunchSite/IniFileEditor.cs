99887c4668be3e8d57a4f7afaeef078aeb9c35b8
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-03-16 11:41:57 +0900

    Revert "GetPrivateProfileString()、GetPrivateProfileInt()を定義"
    This reverts commit d2f1b40a3c73eb12c2ea75fff93962909b9cc230.

--- ThLaunchSite/IniFileEditor.cs
+++ ThLaunchSite/IniFileEditor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,23 +8,5 @@ namespace ThLaunchSite
 {
     internal class IniFileEditor
     {
-        //---DLL関数の定義 START---//
-        //GetPrivateProfileString関数の定義
-        [DllImport("KERNEL32.DLL")]
-        public static extern uint GetPrivateProfileString(
-            string lpAppName,
-            string lpKeyName,
-            string lpDefault,
-            StringBuilder lpReturnedString,
-            uint nSize,
-            string lpFileName);
-
-        //GetPrivateProfileInt関数の定義
-        [DllImport("KERNEL32.DLL")]
-        public static extern uint GetPrivateProfileInt(
-            string lpAppName,
-            string lpKeyName,
-            int nDefault,
-            string lpFileName);
     }
 }
