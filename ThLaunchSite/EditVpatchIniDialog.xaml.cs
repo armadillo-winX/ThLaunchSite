201f204a75a04d5f30132d702c8dc2e6dced16d5
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-03-16 13:20:44 +0900

    vpatch.ini編集ダイアログを実装

--- ThLaunchSite/EditVpatchIniDialog.xaml.cs
+++ ThLaunchSite/EditVpatchIniDialog.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Text;
 
 namespace ThLaunchSite
 {
@@ -22,6 +10,73 @@ namespace ThLaunchSite
         public EditVpatchIniDialog()
         {
             InitializeComponent();
+        }
+
+        private string? _vsyncPatchIniFilePath;
+
+        public string? VsyncPatchIniFilePath 
+        {
+            get
+            {
+                return _vsyncPatchIniFilePath;
+            }
+
+            set
+            {
+                _vsyncPatchIniFilePath = value;
+                GetVsyncPatchIniData(value);
+            }
+        }
+
+        private void GetVsyncPatchIniData(string vsyncPatchIniFilePath)
+        {
+            if (File.Exists(vsyncPatchIniFilePath))
+            {
+                try
+                {
+                    StreamReader streamReader = new(vsyncPatchIniFilePath, Encoding.UTF8);
+                    string data = streamReader.ReadToEnd();
+                    streamReader.Close();
+
+                    EditorBox.Text = data;
+                }
+                catch (Exception ex)
+                {
+                    EditorBox.IsReadOnly = true;
+                    EditorBox.Text = $"vpatch.iniの読み込みに失敗。\n{ex.Message}";
+                    SaveButton.IsEnabled = false;
+                }
+            }
+            else
+            {
+                EditorBox.IsReadOnly = true;
+                EditorBox.Text = "vpatch.iniが見つかりませんでした。";
+                SaveButton.IsEnabled = false;
+            }
+        }
+
+        private void SaveVsyncPatchIniData(string data)
+        {
+            File.WriteAllText(data, this.VsyncPatchIniFilePath);
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveVsyncPatchIniData(EditorBox.Text);
+                MessageBox.Show(this, "保存しました。", "ファイルの保存",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, $"ファイルの保存に失敗。\n{ex.Message}", "エラー",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                this.Close();
+            }
         }
     }
 }
