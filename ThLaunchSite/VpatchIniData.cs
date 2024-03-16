70a167e7e20761993d2d1c2a572364ff34786964
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-03-16 11:41:25 +0900

    Revert "VpatchIniData実装"
    This reverts commit 9e166cb7c903c5f7fcfb760b9009c8fd80bedc46.

--- ThLaunchSite/VpatchIniData.cs
+++ ThLaunchSite/VpatchIniData.cs
@@ -1,43 +1,12 @@
-﻿namespace ThLaunchSite
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThLaunchSite
 {
     internal class VpatchIniData
     {
-        public bool IsWindowSelectionEnabled { get; set; }
-
-        public bool IsWindowOperationEnabled { get; set; }
-
-        public int WidnowPositionX { get; set; }
-
-        public int WidnowPositionY { get; set; }
-
-        public int WindowWidth { get; set; }
-
-        public int WindowHeight { get; set; }
-
-        public bool IsTitleBarHidden { get; set; }
-
-        public bool IsWindowAlwaysOnTop { get; set; }
-
-        public int VsyncType { get; set; }
-
-        public int SleepType { get; set; }
-
-        public int BltPrepareTime { get; set; }
-
-        public bool IsAutoBltPrepareTime { get; set; }
-
-        public int GameFps { get; set; }
-
-        public int ReplaySkipFps { get; set; }
-
-        public int ReplaySlowFps { get; set; }
-
-        public bool IsFpsCalculationEnabled { get; set; }
-
-        public bool IsAlwaysBlt { get; set; }
-
-        public bool IsFixCherryBugEnabled { get; set; }
-
-        public bool IsFixBugMarisaEnabled { get; set; }
     }
 }
