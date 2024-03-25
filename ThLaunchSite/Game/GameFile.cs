825ece04bef42248fe71fe76756206bf9bb30dc1
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-03-26 00:13:37 +0900

    実装をより簡素化

--- ThLaunchSite/Game/GameFile.cs
+++ ThLaunchSite/Game/GameFile.cs
@@ -1,128 +1,37 @@
-﻿namespace ThLaunchSite.Game
+﻿using System.Reflection;
+
+namespace ThLaunchSite.Game
 {
     internal class GameFile
     {
         public static string? GetGameFilePath(string gameId)
         {
-            if (gameId == GameIndex.Th06)
+            //プロパティ名からプロパティを取得
+            PropertyInfo? gamePathProperty = typeof(GamePath).GetProperty($"{gameId}FilePath");
+            string? gamePath;
+            if (gamePathProperty != null)
             {
-                return GamePath.Th06FilePath;
-            }
-            else if (gameId == GameIndex.Th07)
-            {
-                return GamePath.Th07FilePath;
-            }
-            else if (gameId == GameIndex.Th08)
-            {
-                return GamePath.Th08FilePath;
-            }
-            else if (gameId == GameIndex.Th09)
-            {
-                return GamePath.Th09FilePath;
-            }
-            else if (gameId == GameIndex.Th10)
-            {
-                return GamePath.Th10FilePath;
-            }
-            else if (gameId == GameIndex.Th11)
-            {
-                return GamePath.Th11FilePath;
-            }
-            else if (gameId == GameIndex.Th12)
-            {
-                return GamePath.Th12FilePath;
-            }
-            else if (gameId == GameIndex.Th13)
-            {
-                return GamePath.Th13FilePath;
-            }
-            else if (gameId == GameIndex.Th14)
-            {
-                return GamePath.Th14FilePath;
-            }
-            else if (gameId == GameIndex.Th15)
-            {
-                return GamePath.Th15FilePath;
-            }
-            else if (gameId == GameIndex.Th16)
-            {
-                return GamePath.Th16FilePath;
-            }
-            else if (gameId == GameIndex.Th17)
-            {
-                return GamePath.Th17FilePath;
-            }
-            else if (gameId == GameIndex.Th18)
-            {
-                return GamePath.Th18FilePath;
-            }
-            else if (gameId == GameIndex.Th19)
-            {
-                return GamePath.Th19FilePath;
+                gamePath 
+                = gamePathProperty.GetValue(null, null) != null ?
+                gamePathProperty.GetValue(null, null).ToString() :
+                string.Empty;
             }
             else
             {
-                return null;
+                gamePath = string.Empty;
             }
+
+            return gamePath;
         }
 
         public static void SetGameFilePath(string gameId, string gamePath)
         {
-            if (gameId == GameIndex.Th06)
+            //プロパティ名からプロパティを取得
+            PropertyInfo? gamePathProperty = typeof(GamePath).GetProperty($"{gameId}FilePath");
+            if (gamePathProperty != null)
             {
-                GamePath.Th06FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th07)
-            {
-                GamePath.Th07FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th08)
-            {
-                GamePath.Th08FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th09)
-            {
-                GamePath.Th09FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th10)
-            {
-                GamePath.Th10FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th11)
-            {
-                GamePath.Th11FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th12)
-            {
-                GamePath.Th12FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th13)
-            {
-                GamePath.Th13FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th14)
-            {
-                GamePath.Th14FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th15)
-            {
-                GamePath.Th15FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th16)
-            {
-                GamePath.Th16FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th17)
-            {
-                GamePath.Th17FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th18)
-            {
-                GamePath.Th18FilePath = gamePath;
-            }
-            else if (gameId == GameIndex.Th19)
-            {
-                GamePath.Th19FilePath = gamePath;
+                //取得したプロパティに値を代入
+                gamePathProperty.SetValue(null, gamePath);
             }
         }
 
