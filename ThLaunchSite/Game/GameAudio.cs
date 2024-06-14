4934fb14bc6db394738701a80b72a3f004f9e148
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-06-15 03:36:07 +0900

    GameAudio 追加

---
+++ ThLaunchSite/Game/GameAudio.cs
@@ -0,0 +1,13 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThLaunchSite.Game
+{
+    internal class GameAudio
+    {
+    }
+}
